2927976ade1ff5a9d18f9fd62756f9a3716071d1
nipoon777 <nipoon.donta7@gmail.com>
2022-12-02 09:11:39 -0800

    QRCode fixed

--- client/src/Components/QRCodeScanner.js
+++ client/src/Components/QRCodeScanner.js
@@ -1,47 +1,36 @@
-import PropTypes from 'prop-types'
-// import React, { Component } from 'react'
-
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import QrReader from 'react-qr-scanner'
+import { useNavigate } from "react-router-dom";
 
-class QRCodeScanner extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      delay: 100,
-      result: '',
-    }
 
-    this.handleScan = this.handleScan.bind(this)
-  }
-  handleScan(data){
-    this.setState({
-      result: data,
-    })
-    console.log("bruh")
-    // window.open("/artists/")
-  }
-  handleError(err){
-    console.error(err)
-  }
-  render(){
-    const previewStyle = {
-      height: 550,
-      width: 550,
-    }
+function QRCodeScanner() {
+  const [data, setData] = useState('No result');
+  const history = useNavigate();
+    useEffect(()=>{
+        if(data==='No result') {
+            return;
+        }
+        let link = data.split("/");
+        let newlink = link[link.length - 2] + '/' + link[link.length - 1];
+        history(`/${newlink}`);
+    }, [data])
 
-    return(
+  return (
       <div style={{display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'center', marginTop: '50px'}}>
-        <QrReader
-          delay={this.state.delay}
-          style={previewStyle}
-          onError={this.handleError}
-          onScan={this.handleScan}
-          />
-        <p>{this.state.result}</p>
-      </div>
-    )
-  }
+      <QrReader
+        onScan= {(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        />
+      <p> {data}</p>
+    </div>
+  )
 }
 
 export default QRCodeScanner
