99a429b6540423cf31e01f218819120827471c6e
Umang Sahastransu <usahastr@asu.edu>
2022-12-02 20:58:54 -0700

    Server ip changed

--- server/src/main/java/com/ser531/team7/smartmuseumtour/config/production.java
+++ server/src/main/java/com/ser531/team7/smartmuseumtour/config/production.java
@@ -1,7 +1,7 @@
 package com.ser531.team7.smartmuseumtour.config;
 
 public class production {
-    public static final String ARTWORK_DS_URI = "http://35.89.87.20:3030/persistent/query";
-    public static final String ARTIST_DS_URI = "http://34.221.188.174:3030/persistent/query";
-    public static final String EXHIBITION_DS_URI = "http://54.186.12.151:3030/persistent/query";
+    public static final String ARTWORK_DS_URI = "http://35.92.111.30:3030/persistent/query";
+    public static final String ARTIST_DS_URI = "http://35.92.163.71:3030/persistent/query";
+    public static final String EXHIBITION_DS_URI = "http://35.167.140.164:3030/persistent/query";
 }
